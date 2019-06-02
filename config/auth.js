1389b9e34701e087869408904e8386a4b527309e
TatianaChoisy <choisytatiana@gmail.com>
2019-06-02 15:41:13 +0200

    Authentification Facebook, création d'un utilisateur et utilisation de EJS pour mes templates

---
+++ config/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'facebookAuth : {
+        AppID: '1115663225284301',
+    AppSecret: '29bc02f9d5efe6bfe62bb766166c7b95',
+    callbackURL: 'http://localhost:8080/auth/Facebook/callback'
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+// Passport est un middleware d'authentification pour Node. Il est conçu pour servir un objectif unique: authentifier les demandes. Lors de l'écriture de modules, l'encapsulation est une vertu. Passport délègue toutes les autres fonctionnalités à l'application. Cette séparation des problèmes maintient le code propre et maintenable, et rend Passport extrêmement facile à intégrer dans une application.
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'anystringoftext',
+				 saveUninitialized: true,
+				 resave: true}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // 
+app.use(flash()); 
+
+
+
+
+app.set('view engine', 'ejs');
+
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('Server running on port: ' + port);
+
+
