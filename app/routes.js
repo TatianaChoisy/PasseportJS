dbbf7ff6ec5c3f51118172f7e7f1d8f2aa63290f
TatianaChoisy <choisytatiana@gmail.com>
2019-06-02 19:08:44 +0200

    ID et password retirés pour confidentialité

--- app/routes.js
+++ app/routes.js
@@ -31,8 +31,10 @@ module.exports = function(app, passport){
 	app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
 
 	app.get('/auth/facebook/callback', 
-	  passport.authenticate('facebook', { successRedirect: '/profile',
-	                                      failureRedirect: '/' }));
+		passport.authenticate('facebook', {
+			successRedirect: '/profile',
+			failureRedirect: '/'
+		}));
 
 
 	app.get('/logout', function(req, res){
